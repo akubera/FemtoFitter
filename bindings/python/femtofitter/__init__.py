1a6101b501060de181b9d9943dd9a9ded6ad5097
Andrew Kubera <andrew.michael.kubera@cern.ch>
2018-05-28 20:44:19 -0400

    python-bindings: Use pathlib to find FemtoFitter library

--- bindings/python/femtofitter/__init__.py
+++ bindings/python/femtofitter/__init__.py
@@ -3,10 +3,15 @@
 #
 
 import os
+from pathlib import Path
 
 LIBFEMTOFITTER_PATH = os.environ.get("LIBFEMTOFITTER")
 
 if not LIBFEMTOFITTER_PATH:
+    libfem = Path(__file__).parent
+else:
+    libfem = Path(LIBFEMTOFITTER_PATH)
+
 
 
 
