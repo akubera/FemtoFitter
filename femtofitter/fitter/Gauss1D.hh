e96488f2279349ca7ba48cf20b090d7a8cde217c
Andrew Kubera <andrew.michael.kubera@cern.ch>
2018-09-23 23:32:58 -0400

    Format Gauss1D

--- femtofitter/fitter/Gauss1D.hh
+++ femtofitter/fitter/Gauss1D.hh
@@ -9,6 +9,7 @@
 #include <array>
 #include <string>
 #include "../inc/femtomath.h"
+#include "../inc/value.h"
 
 
 /// Calculate Gaussian factor for 1D system
@@ -32,7 +33,6 @@ double calculate_gauss1d(double qinv, double RinvSq, double lam, double K=1.0, d
     result = (1.0 - lam) + lam * K * gauss;
 
   return norm * result;
-
 }
 
 
@@ -63,9 +63,13 @@ struct Gauss1DT {
   struct FitParams;
   struct FitInput;
   struct FitResult;
+  struct FitData;
 
-  static std::string GetName() { return "Gauss1D"; }
-  static size_t GetNparams() { return N + 3; }
+  static std::string GetName()
+    { return "Gauss1D"; }
+
+  static size_t GetNparams()
+    { return N + 3; }
 
   enum {
     DATAPTR_PARAM_IDX = 0,
@@ -75,149 +79,154 @@ struct Gauss1DT {
     NORM_PARAMS_IDX = 3,
   };
 
-  struct FitParams {
-    double lam,
-           radius;
-    std::array<double N> norm;
-
-    FitParams(double *par)
-      : norm(par+NORM_PARAMS_IDX, par+NORM_PARAMS_IDX+N)
-      , lam(par[LAM_PARAM_IDX])
-      , radius(par[R_PARAM_IDX])
-    {
-    }
-
-    bool is_invalid() const
-    {
-        return radius < 0
-            || lam < 0
-            || std::isnan(radius)
-            || std::isnan(lam);
-    }
-  };
-
-  struct Value {
-      double value;
-      double error;
-  };
-
-  /// \class FitData
-  /// \brief Histograms to pass to the fitter object
-  struct FitData : GenericInputData<FitData> {
-    using Super = GenericInputData<FitData>;
-
-    using HistPtr_t = TH1*;
-
-    std::array<HistPtr_t, N> num;
-    std::array<HistPtr_t, N> den;
-    std::array<HistPtr_t, N> qinv;
-
-    /// Cache of q-values in histograms
-    std::vector<double> q_vec;
+};
 
 
-    /// Build with pairs of numerators and denominators
-    FitData(std::array<HistPtr_t, N> nums, std::array<HistPtr_t, N> dens)
-     : FitData(nums, dens, {nullptr})
-    { }
-
-    FitData(std::array<HistPtr_t, N> nums, std::array<HistPtr_t, N> dens, std::array<HistPtr_t, N> qinvs)
-     : num(nums)
-     , den(dens)
-     , qinv(qinvs)
-    {
-      num[0]->GetXaxis()->GetCenter(q_vec.data()+1);
-    }
-
-    void SetupMinuit(TMinuit &minuit)
-    {
-      int errflag = Super::SetupMinuit(minuit);
-      minuit.mnparm(LAM_PARAM_IDX, "Lam", 0.5, 0.01, 0.0, 0.0, errflag);
-      minuit.mnparm(R_PARAM_IDX, "Radius", 5, 0.2, 0.0, 0.0, errflag);
-
-      for (int i = NORM_PARAMS_IDX, STOP=i+N; i < STOP; ++i) {
-        minuit.mnparam(i, Form("Norm%d", i-R_PARAM_IDX), 0.2, 0.02, 0.0, 0.0, errflag);
-      }
-
-      if (errflag != 0) {
-        std::cerr << "Error setting paramters: " << errflag << "\n";
-        throw std::runtime_error("Could not set Minuit parameters.");
-      }
-    }
-
-    FitResult PerformFit(TMinuit &minuit)
-    {
-      SetupMinuit(minuit);
-      minuit.SetFCN(fit_sans_coulomb);
-      minuit.Migrad();
-      return FitResult(minuit);
-    }
-
-    double SetFitBound(double fit)
-    {
-    }
-
-    static double fit_sans_coulomb(const FitParams &param)
-    {
-      double retval = 0;
-      for (size_t idx=0; idx < N; ++idx) {
-
-        const auto &n = *num[idx],
-                   &d = *den[idx];
-        const double norm = param.norm[idx];
-
-        for (int bin = 0; bin < 10; ++bin) {
-          const double A = n->GetBinContent(bin),
-                       B = d->GetBinContent(bin),
-                       C = calculate(q_vec[bin], param.radius * param.radius, 1.0, norm);
-
-          retval += loglikelihood_calc(A, B, C);
-        }
-      }
-
-      return -2 * retval;
-    }
-
-    static double fit_coulomb(const FitParams &param)
-    {
-      double retval = 0;
-      auto coulomb_factor = CoulombHist::GethistWithRadius(param.radius);
-
-      for (size_t idx=0; idx < N; ++idx) {
-
-        const auto &n = *num[idx],
-                   &d = *den[idx],
-                   &qinv_hist = *qinv[idx];
-        const double norm = param.norm[idx];
-
-        for (int bin = 0; bin < 10; ++bin) {
-          const double A = n->GetBinContent(bin),
-                       B = d->GetBinContent(bin),
-                       C = calculate(q_vec[bin], param.radius * param.radius, K, norm);
-
-          retval += loglikelihood_calc(A, B, C);
-        }
-      }
-
-      return -2 * retval;
-    }
-
-  };
-
-
-  struct FitResult {
-    Value lam,
-          radius;
-    std::array<Value, N> norms;
-
-    FitResult(TMinuit &minuit)
-    {
-
-    }
-
-  };
+/// \class Gauss1D::FitResult
+/// \brief Result of the fit
+template <typename T>
+struct Gauss1DT<T>::FitResult {
+  Value lam,
+        radius;
+  std::array<Value, N> norms;
 
 };
 
+
+/// \class FitData
+/// \brief Histograms to pass to the fitter object
+///
+struct FitData : GenericInputData<FitData> {
+  using Super = GenericInputData<FitData>;
+
+  using HistPtr_t = TH1*;
+
+  std::array<HistPtr_t, N> num;
+  std::array<HistPtr_t, N> den;
+  std::array<HistPtr_t, N> qinv;
+
+  /// Cache of q-values in histograms
+  std::vector<double> q_vec;
+
+
+  /// Build with pairs of numerators and denominators
+  FitData(std::array<HistPtr_t, N> nums, std::array<HistPtr_t, N> dens)
+   : FitData(nums, dens, {nullptr})
+  { }
+
+  FitData(std::array<HistPtr_t, N> nums, std::array<HistPtr_t, N> dens, std::array<HistPtr_t, N> qinvs)
+    : num(nums)
+    , den(dens)
+    , qinv(qinvs)
+  {
+    std::get<0>(num)->GetXaxis()->GetCenter(q_vec.data()+1);
+  }
+
+  void
+  SetupMinuit(TMinuit &minuit)
+  {
+    int errflag = Super::SetupMinuit(minuit);
+    minuit.mnparm(LAM_PARAM_IDX, "Lam", 0.5, 0.01, 0.0, 0.0, errflag);
+    minuit.mnparm(R_PARAM_IDX, "Radius", 5, 0.2, 0.0, 0.0, errflag);
+
+    for (int i = NORM_PARAMS_IDX, STOP=i+N; i < STOP; ++i) {
+      minuit.mnparam(i, Form("Norm%d", i-R_PARAM_IDX), 0.2, 0.02, 0.0, 0.0, errflag);
+    }
+
+    if (errflag != 0) {
+      std::cerr << "Error setting paramters: " << errflag << "\n";
+      throw std::runtime_error("Could not set Minuit parameters.");
+    }
+  }
+
+  FitResult
+  PerformFit(TMinuit &minuit)
+  {
+    SetupMinuit(minuit);
+    minuit.SetFCN(fit_sans_coulomb);
+    minuit.Migrad();
+    return FitResult(minuit);
+  }
+
+  double
+  SetFitBound(double fit)
+  {
+
+  }
+
+  static double
+  fit_sans_coulomb(const FitParams &param)
+  {
+    double retval = 0;
+    for (size_t idx=0; idx < N; ++idx) {
+
+      const auto &n = *num[idx],
+                 &d = *den[idx];
+      const double norm = param.norm[idx];
+
+      for (int bin = 0; bin < 10; ++bin) {
+        const double A = n->GetBinContent(bin),
+                     B = d->GetBinContent(bin),
+                     C = calculate(q_vec[bin], param.radius * param.radius, 1.0, norm);
+
+        retval += loglikelihood_calc(A, B, C);
+      }
+    }
+
+    return -2 * retval;
+  }
+
+  static double
+  fit_coulomb(const FitParams &param)
+  {
+    double retval = 0;
+    auto coulomb_factor = CoulombHist::GethistWithRadius(param.radius);
+
+    for (size_t idx=0; idx < N; ++idx) {
+
+      const auto &n = *num[idx],
+                 &d = *den[idx],
+                 &qinv_hist = *qinv[idx];
+      const double norm = param.norm[idx];
+
+      for (int bin = 0; bin < 10; ++bin) {
+        const double A = n->GetBinContent(bin),
+                     B = d->GetBinContent(bin),
+                     C = calculate(q_vec[bin], param.radius * param.radius, K, norm);
+
+        retval += loglikelihood_calc(A, B, C);
+      }
+    }
+
+    return -2 * retval;
+  }
+};
+
+
+/// \class FitParams
+/// \brief
+///
+template <typename T>
+struct Gauss1DT<T>::FitParams {
+  double lam,
+         radius;
+  std::array<double N> norm;
+
+  FitParams(double *par)
+    : norm(par+NORM_PARAMS_IDX, par+NORM_PARAMS_IDX+N)
+    , lam(par[LAM_PARAM_IDX])
+    , radius(par[R_PARAM_IDX])
+  {
+  }
+
+  bool
+  is_invalid() const
+  {
+    return radius < 0
+        || lam < 0
+        || std::isnan(radius)
+        || std::isnan(lam);
+  }
+};
 
 #endif
