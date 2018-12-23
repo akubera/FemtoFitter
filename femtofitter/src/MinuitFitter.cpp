5ea4069d5f6abea8118b742fe60bfc068a90b105
Andrew Kubera <andrew.michael.kubera@cern.ch>
2018-12-23 00:36:21 -0500

    Add class MinuitFitter

---
+++ femtofitter/src/MinuitFitter.cpp
@@ -0,0 +1,12 @@
+///
+/// \file femtofitter/MinuitFitter.cpp
+///
+
+
+#include "MinuitFitter.hpp"
+
+/*
+MinuitFitter::MinuitFitter()
+  : fMinuit(std::make_unique<TMinuit>())
+{}
+*/
---
+++ femtofitter/src/MinuitFitter.hpp
@@ -0,0 +1,43 @@
+///
+/// \file femtofitter/MinuitFitter.hpp
+///
+
+
+#ifndef MINUITFITTER_HPP
+#define MINUITFITTER_HPP
+
+
+#include <memory>
+#include <TMinuit.h>
+
+/// \class MinuitFitter
+/// \brief Add
+class MinuitFitter {
+
+  std::unique_ptr<TMinuit> fMinuit;
+
+public:
+
+  /// MinuitFitter
+  // MinuitFitter();
+
+  template <typename T>
+  MinuitFitter();
+};
+
+template <typename T>
+MinuitFitter::MinuitFitter()
+  : fMinuit(std::make_unique<TMinuit>())
+{
+  T a;
+  a.setup_minuit(*fMinuit);
+}
+
+
+
+#include "../../src/FitterLevy.hpp"
+
+template <>
+MinuitFitter::MinuitFitter<FitterLevy>();
+
+#endif
