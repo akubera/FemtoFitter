49556d3aeb67e85df8f5c0caf0c4040559b91647
Andrew Kubera <andrew.michael.kubera@cern.ch>
2019-01-04 18:12:36 -0500

    Linkdef: Improve Fitter3D<> subclass formatting

--- Linkdef.h
+++ Linkdef.h
@@ -1,16 +1,25 @@
 
+#ifdef __ROOTCLING__
+
+#pragma link off all class;
+
+#pragma link C++ class Data1D;
 #pragma link C++ class Data3D;
 
-#pragma link C++ class FitterGaussOSL;
-#pragma link C++ class FitterGaussOSL::Data;
+#pragma link C++ class Fitter3D<FitterGaussOSL>;
 #pragma link C++ class FitterGaussOSL::FitParams;
 #pragma link C++ class FitterGaussOSL::FitResult;
 
-#pragma link C++ class FitterLevy;
-#pragma link C++ class FitterLevy::Data;
+#pragma link C++ class Fitter3D<FitterGaussFull>;
+#pragma link C++ class FitterGaussFull::FitParams;
+#pragma link C++ class FitterGaussFull::FitResult;
+
+#pragma link C++ class Fitter3D<FitterLevy>;
 #pragma link C++ class FitterLevy::FitParams;
 #pragma link C++ class FitterLevy::FitResult;
 
 #pragma link C++ function apply_momentum_resolution_correction;
 
 #pragma link C++ class CoulombHist;
+
+#endif
