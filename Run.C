dfc3b2bf8592cc6d3e9f0edd1d2ef2b7bb887c9a
Andrew Kubera <andrew.michael.kubera@cern.ch>
2019-01-24 13:30:32 -0500

    Run.C: Vary ErrorDef minuit setting

--- Run.C
+++ Run.C
@@ -1,25 +1,59 @@
 
 R__LOAD_LIBRARY(build/libFemtoFitter.so)
 
-#include "femtofitter/MomentumResolutionCorrector.hpp"
-
 
 void
 Run()
 {
-  std::cout << "Run\n";
+  std::cout << "Running\n";
 
-  // gSystem->Load("build/libFemtoFitter.so");
-
-  std::cout << "FemtoFitter\n";
-
-  MomentumResolutionCorrector mrc;
+  // MomentumResolutionCorrector mrc;
   // mrc.a[{0, 0, 0}][4] = 4.5;
 
 
-  TFile f("mrc.root", "RECREATE");
-  f.WriteTObject(&mrc);
+  // TFile f("mrc.root", "RECREATE");
+  // f.WriteTObject(&mrc);
   // std::cout << mrc.a[{0,0,0}][4] << "\n";
 
+  auto tfile = TFile::Open("Data-Cmp.root");
+  if (!tfile) {
+    return 1;
+  }
+
+  tfile->ls();
+
+  auto *tdir = (TDirectory*)tfile->Get("Q3DLCMS/cfg2962DF8ABB076C96/pim/00_05/0.2_0.3/--");
+  auto *mrc = (TH3*)tfile->Get("AnalysisTrueQ3D/cfgBDC0F09B1F286D46/pim/00_90/0.2_0.3/--/mrc");
+
+  auto data = Data3D::FromDirectory(*tdir, mrc, 0.13);
+
+  FitterGaussOSL fitter(*data);
+
+  TMinuit minuit;
+  fitter.setup_pml_fitter(minuit);
+  minuit.SetErrorDef(1.0);
+  minuit.Migrad();
+
+  TMinuit minuit1;
+  fitter.setup_pml_fitter(minuit1);
+  minuit1.SetErrorDef(0.5);
+  minuit1.Migrad();
+
+
+  TMinuit minuit2;
+  fitter.setup_pml_fitter(minuit2);
+  minuit2.SetErrorDef(5.88);
+  minuit2.Migrad();
+
+  TMinuit minuit3;
+  fitter.setup_pml_fitter(minuit3);
+  minuit3.SetErrorDef(5.88 / 2);
+  minuit3.Migrad();
+
+
+  minuit.mnprin(4, 1.0);
+  minuit1.mnprin(4, 1.0);
+  minuit2.mnprin(4, 1.0);
+  minuit3.mnprin(4, 1.0);
 
 }
