7ee8093797fcf777e8e0cc5dffbc6e80c2faa1f7
Andrew Kubera <andrew.michael.kubera@cern.ch>
2019-03-15 23:18:50 -0400

    FitterGaussOSL: Fix calculation of Rinv

--- femtofitter/fitter/FitterGaussOSL.hpp
+++ femtofitter/fitter/FitterGaussOSL.hpp
@@ -166,7 +166,7 @@ struct FitterGaussOSL : public Fitter3D<FitterGaussOSL> {
 
     /// Return calculated Rinv: $\sqrt{Ro^2 \gamma + Rs^2 + Rl^2}$
     double PseudoRinv(double gamma) const
-      { return std::sqrt((Ro * Ro * gamma + Rs * Rs + Rl * Rl) / 3.0); }
+      { return std::sqrt((Ro * Ro * gamma * gamma + Rs * Rs + Rl * Rl) / 3.0); }
 
     double gauss(const std::array<double, 3> &q, double K) const
       { return FitterGaussOSL::gauss(q, {Ro*Ro, Rs*Rs, Rl*Rl}, lam, K, norm); }
