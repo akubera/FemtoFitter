ed8596cc895f27cc7eb8fb3ad6e5c7d8495f5b4f
Andrew Kubera <andrew.michael.kubera@cern.ch>
2019-03-22 18:32:59 -0400

    Add cut-configuration panel

---
+++ femtofitter/src/bin/fit-inspector/PanelCutConfig.hpp
@@ -0,0 +1,24 @@
+///
+/// \file fit-inspector/PanelCutConfig.hpp
+///
+
+
+#pragma once
+
+#ifndef PANELCUTCONFIG_HPP
+#define PANELCUTCONFIG_HPP
+
+
+/// \class PanelCutConfig
+/// \brief Display fit parameters
+///
+class PanelCutConfig {
+
+public:
+
+  PanelCutConfig()
+    {}
+};
+
+
+#endif
