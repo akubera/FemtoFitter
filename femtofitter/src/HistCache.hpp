cb4f90f50e74de05c9f71282aa0c5d8e1c8f7439
Andrew Kubera <andrew.michael.kubera@cern.ch>
2019-06-18 16:33:54 -0400

    HistCache: Reduce implementation details

--- femtofitter/src/HistCache.hpp
+++ femtofitter/src/HistCache.hpp
@@ -15,33 +15,26 @@
 #include <TH3.h>
 
 
-/// \class HistCachce
-template <typename HistType, typename OutHist=HistType>
-struct HistCache {
+template <typename T>
+struct KeyType;
 
-  struct Key_t;
-  // std::map<std::tuple<int, double, double>, std::unique_ptr<HistType>> hist;
-  std::map<Key_t, std::shared_ptr<HistType>> storage;
-
-
-  std::shared_ptr<OutHist> operator[](const HistType &);
-
-  void insert(const HistType &, std::shared_ptr<OutHist>);
-
-  static Key_t make_key(const HistType &hist);
-
-};
 
 template <>
-struct HistCache<TH3>::Key_t {
+struct KeyType<TH3> {
   std::string classname;
   std::tuple<int, double, double>
     x_axis,
     y_axis,
     z_axis;
 
+  KeyType(const TH3 &h)
+    : classname(h.GetName())
+    , x_axis(std::make_tuple(h.GetNbinsX(), h.GetXaxis()->GetXmin(), h.GetXaxis()->GetXmax()))
+    , y_axis(std::make_tuple(h.GetNbinsY(), h.GetYaxis()->GetXmin(), h.GetYaxis()->GetXmax()))
+    , z_axis(std::make_tuple(h.GetNbinsZ(), h.GetZaxis()->GetXmin(), h.GetZaxis()->GetXmax()))
+    {}
 
-  bool operator<(const Key_t &rhs) const
+  bool operator<(const KeyType &rhs) const
     {
       return classname < rhs.classname
           && x_axis < rhs.x_axis
@@ -53,98 +46,62 @@ struct HistCache<TH3>::Key_t {
 
 
 template <>
-struct HistCache<TH1, TH1D>::Key_t {
+struct KeyType<TH1> {
   std::string classname;
   std::tuple<int, double, double> x_axis;
 
-  bool operator<(const Key_t &rhs) const
+  KeyType(const TH1 &h)
+    : classname(h.GetName())
+    , x_axis(std::make_tuple(h.GetNbinsX(), h.GetXaxis()->GetXmin(), h.GetXaxis()->GetXmax()))
+    {}
+
+  bool operator<(const KeyType &rhs) const
     {
       return classname < rhs.classname && x_axis < rhs.x_axis;
     }
 };
 
+/// \class HistCachce
+/// \brief Cache histograms based on axes
+///
+template <typename HistType, typename OutHistType=HistType>
+struct HistCache {
 
-template <>
-struct HistCache<TH1, TH2D>::Key_t {
-  std::string classname;
-  std::tuple<int, double, double> x_axis;
+  using Key_t = KeyType<HistType>;
 
-  bool operator<(const Key_t &rhs) const
+  // std::map<std::tuple<int, double, double>, std::unique_ptr<HistType>> hist;
+  std::map<Key_t, std::shared_ptr<OutHistType>> storage;
+
+  std::shared_ptr<OutHistType> operator[](const HistType &h)
     {
-      return classname < rhs.classname && x_axis < rhs.x_axis;
+      return get(h);
     }
+
+  std::shared_ptr<OutHistType> get(const HistType &h)
+    {
+      Key_t key(h);
+
+      auto found = storage.find(key);
+      if (found == storage.end()) {
+        return nullptr;
+      }
+
+      return found->second;
+    }
+
+  void
+  insert(const HistType &keyhist, std::shared_ptr<OutHistType> hist)
+    {
+      Key_t key(keyhist);
+      storage.emplace(key, hist);
+    }
+
+  static Key_t make_key(const HistType &hist)
+    {
+      return Key_t(hist);
+    }
+
 };
 
-
-template <>
-struct HistCache<TH1>::Key_t {
-  std::string classname;
-  std::tuple<int, double, double> x_axis;
-
-  bool operator<(const Key_t &rhs) const
-    {
-      return classname < rhs.classname && x_axis < rhs.x_axis;
-    }
-};
-
-
-template <>
-auto HistCache<TH3>::make_key(const TH3 &h) -> Key_t
-{
-  const TAxis
-    &xax = *h.GetXaxis(),
-    &yax = *h.GetYaxis(),
-    &zax = *h.GetZaxis();
-
-  const std::string classname = h.ClassName();
-
-  const auto
-    x = std::make_tuple(xax.GetNbins(), xax.GetXmin(), xax.GetXmax()),
-    y = std::make_tuple(yax.GetNbins(), yax.GetXmin(), yax.GetXmax()),
-    z = std::make_tuple(zax.GetNbins(), zax.GetXmin(), zax.GetXmax());
-
-  Key_t key = { h.ClassName(), x, y, z };
-  // std::string classname;
-  // std::tuple<int, double, double>
-  return key;
-}
-
-template <>
-auto HistCache<TH1>::make_key(const TH1 &h) -> Key_t
-{
-  const TAxis &xax = *h.GetXaxis();
-
-  const std::string classname = h.ClassName();
-
-  const auto x = std::make_tuple(xax.GetNbins(), xax.GetXmin(), xax.GetXmax());
-
-  Key_t key = { h.ClassName(), x };
-
-  return key;
-}
-
-
-template<>
-inline auto
-HistCache<TH3>::operator[](const TH3 &h) -> std::shared_ptr<TH3>
-{
-  Key_t key = make_key(h);
-
-  auto found = storage.find(key);
-  if (found == storage.end()) {
-    return nullptr;
-  }
-
-  return found->second;
-}
-
-
-template<typename HistType, typename OutHistType>
-inline void
-HistCache<HistType, OutHistType>::insert(const HistType &keyhist, std::shared_ptr<OutHistType> hist)
-{
-  auto key = make_key(keyhist);
-  storage.emplace(key, hist);
-}
 
 #endif
