ae0c6469089c18398ed0f37e7c76ee50001a4622
Andrew Kubera <andrew.michael.kubera@cern.ch>
2019-07-18 08:17:27 -0400

    FsiKFile: Use safer new_shared_ptr and randomize cache-hist names

--- femtofitter/fsi/FsiKFile.hpp
+++ femtofitter/fsi/FsiKFile.hpp
@@ -13,7 +13,9 @@
 #include <TProfile.h>
 #include <TString.h>
 #include <TFile.h>
+#include <TRandom.h>
 
+#include <iostream>
 #include <vector>
 #include <algorithm>
 
@@ -25,18 +27,32 @@
 struct FsiKFile : public FsiCalculator {
 
   /// Histogram of K-factor qinv vs Rinv
-  std::shared_ptr<const TH2> k2ss;
+  std::shared_ptr<const TH2> k2ss = nullptr;
 
   /// Name of the file storing the histogram
   std::string filename;
 
   /// buffer
-  std::shared_ptr<const TProfile> _qinv_src;
+  std::shared_ptr<const TProfile> _qinv_src = nullptr;
 
   FsiKFile(TString fname)
-    : FsiKFile(*std::make_unique<TFile>(fname))
+    : FsiKFile(std::make_unique<TFile>(fname))
+    { }
+
+  FsiKFile(std::unique_ptr<TH2> k2)
+    : k2ss(std::move(k2))
     {
+      const TAxis &x = *k2ss->GetXaxis();
+      TString random_name("_cache");
+      for (int _i=0; _i<10; ++_i) {
+        random_name += 'a' + static_cast<char>(gRandom->Integer('z' - 'a'));
+      }
+      _qinv_src = std::make_shared<TProfile>(random_name, "", x.GetNbins(), x.GetXmin(), x.GetXmax());
     }
+
+  FsiKFile(std::unique_ptr<TFile> tfile)
+    : FsiKFile(*tfile)
+    { }
 
   FsiKFile(TFile &tfile)
     : k2ss(nullptr)
@@ -48,8 +64,12 @@ struct FsiKFile : public FsiCalculator {
       }
       k2ss.reset(hist);
 
+      TString random_name("_cache");
+      for (int _i=0; _i<10; ++_i) {
+        random_name += 'a' + static_cast<char>(gRandom->Integer('z' - 'a'));
+      }
       const TAxis &x = *hist->GetXaxis();
-      _qinv_src = std::make_shared<TProfile>("_cache", "", x.GetNbins(), x.GetXmin(), x.GetXmax());
+      _qinv_src = std::make_shared<TProfile>(random_name, "", x.GetNbins(), x.GetXmin(), x.GetXmax());
     }
 
   FsiKFile(TFile &tfile, double qbinsize)
@@ -68,15 +88,28 @@ struct FsiKFile : public FsiCalculator {
         qmin = x.GetXmin(),
         qmax = qmin + nbins * qbinsize;
 
-      _qinv_src = std::make_shared<TProfile>("_cache", "", nbins, qmin, qmax);
+      TString random_name("_cache");
+      for (int _i=0; _i<10; ++_i) {
+        random_name += 'a' + static_cast<char>(gRandom->Integer('z' - 'a'));
+      }
+      _qinv_src = std::make_shared<TProfile>(random_name, "", nbins, qmin, qmax);
     }
+
+  FsiKFile(const TH2 &hist)
+    : k2ss(dynamic_cast<TH2*>(hist.Clone()))
+    { }
 
   FsiKFile(const FsiKFile &orig)
     : k2ss(orig.k2ss)
     , filename(orig.filename)
     {
+      TString random_name("_cache");
+      for (int _i=0; _i<10; ++_i) {
+        random_name += 'a' + static_cast<char>(gRandom->Integer('z' - 'a'));
+      }
+
       const TAxis &x = *k2ss->GetXaxis();
-      _qinv_src = std::make_shared<TProfile>("_cache", "", x.GetNbins(), x.GetXmin(), x.GetXmax());
+      _qinv_src = std::make_shared<TProfile>(random_name, "", x.GetNbins(), x.GetXmin(), x.GetXmax());
     }
 
   virtual ~FsiKFile();
@@ -110,18 +143,18 @@ struct FsiKFile : public FsiCalculator {
         Rinv = max_R;
       }
       else if (Rinv < min_R) {
-        Rinv  = min_R;
+        Rinv = min_R;
       }
 
+      const unsigned Npts = 15;
+
       for (int i=1; i <= hist->GetNbinsX(); ++i) {
-        double shift = 1e-5;
+        const double
+          qlo = hist->GetXaxis()->GetBinLowEdge(i),
+          qhi = hist->GetXaxis()->GetBinUpEdge(i),
+          step = (qhi-qlo) / Npts;
 
-        double qlo = hist->GetXaxis()->GetBinLowEdge(i) + shift;
-        double qhi = hist->GetXaxis()->GetBinUpEdge(i) - shift;
-
-        double step = (qhi-qlo) / 39.0;
-
-        for (double qinv = qlo; qinv <= qhi; qinv += step) {
+        for (double qinv = qlo + step / 2.0; qinv <= qhi; qinv += step) {
           Int_t bin = k2.FindBin(qinv, Rinv);
           double K = k2.IsBinOverflow(bin) ? 1.0 : k2.Interpolate(qinv, Rinv);
           hist->Fill(qinv, K);
@@ -138,7 +171,7 @@ struct FsiKFile : public FsiCalculator {
           }
 
           const double sigma = 1.478e-3;
-          const int N = 17;
+          const int N = 5;
 
           double result = 0.0;
           double norm = 0.0;
@@ -189,7 +222,37 @@ struct FsiKFile : public FsiCalculator {
     { return "FsiKFile[" + filename + "]"; }
 
   static std::shared_ptr<FsiCalculator> new_shared_ptr(std::string fname="KFile2.root")
-    { return std::make_shared<FsiKFile>(fname); }
+    {
+      // auto file = std::make_unique<TFile>(fname, "READ");
+      std::unique_ptr<TFile> file(TFile::Open(fname.c_str(), "READ"));
+      if (!file) {
+        std::cerr << "Could not open FSI file '" << fname << "'\n";
+        return nullptr;
+      }
+
+      std::unique_ptr<TObject> obj(file->Get("k2ss"));
+      if (!obj) {
+        std::cerr << "No TObject k2ss found in '" << fname << "'\n";
+        return nullptr;
+      }
+
+      if (!obj->InheritsFrom("TH2")) {
+        std::cerr << "TObject k2ss not a TH2 '" << fname << "'\n";
+        return nullptr;
+      }
+
+      std::unique_ptr<TH2> h(static_cast<TH2*>(obj.release()));
+
+      auto result = std::make_shared<FsiKFile>(std::move(h));
+      result->filename = file->GetName();
+      return result;
+    }
+
+  static std::shared_ptr<FsiCalculator> new_shared_ptr(std::unique_ptr<TFile> file)
+    { return std::make_shared<FsiKFile>(std::move(file)); }
+
+  static std::shared_ptr<FsiCalculator> new_shared_ptr(TFile& file)
+    { return std::make_shared<FsiKFile>(file); }
 };
 
 
