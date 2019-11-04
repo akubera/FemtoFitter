a9b1e831e3cb379453b6393375a91f1038881733
Andrew Kubera <andrew.michael.kubera@cern.ch>
2019-11-03 22:12:49 -0500

    CalculatorFsi - Add new ForRadius method & remove dead code

--- femtofitter/src/CalculatorFsi.hpp
+++ femtofitter/src/CalculatorFsi.hpp
@@ -16,9 +16,12 @@
 
 /// \class FsiQinv
 /// \brief Returns final-state-interaction from qinv
+///
 struct FsiQinv {
+
   virtual ~FsiQinv()
     {}
+
   virtual double operator()(double qinv) = 0;
 };
 
@@ -34,37 +37,16 @@ struct FsiCalculator {
   virtual ~FsiCalculator()
     {}
 
-  // virtual std::unique_ptr<FsiQinv> ForRadius(double Rinv) = 0;
-
-  // virtual double operator()(double R) const = 0;
   virtual std::function<double(double)> ForRadius(double Rinv) = 0;
 
-/*
-  virtual double operator()(const std::array<double, 3> qinv,
-                            const std::array<double, 3> &R,
-                            double gamma) const
+  virtual std::function<double(double)> ForRadius(double Ro,
+                                                  double Rs,
+                                                  double Rl,
+                                                  double gamma=1.0)
     {
-      const double
-        Ro = R[0],
-        Rs = R[1],
-        Rl = R[2],
-        Rinv = std::sqrt((gamma * gamma * Ro*Ro + Rs*Rs + Rl*Rl) / 3);
-
-      return (*this)(qinv, Rinv);
+      double Rinv = std::sqrt(gamma*gamma*Ro*Ro + Rs*Rs + Rl*Rl);
+      return ForRadius(Rinv);
     }
-
-  virtual double operator()(const std::array<double, 3> &R, double gamma) const
-    {
-      const double
-        Ro = R[0],
-        Rs = R[1],
-        Rl = R[2],
-        Rinv = std::sqrt((gamma * gamma * Ro*Ro + Rs*Rs + Rl*Rl) / 3);
-
-      return (*this)(Rinv);
-    }
-*/
-  // virtual double eval(double qinv) const = 0;
 
   virtual std::string ClassName() const = 0;
   virtual std::string Describe() const
@@ -72,34 +54,5 @@ struct FsiCalculator {
       return ClassName();
     }
 };
-
-
-#if false
-template <typename CRTP>
-class FsiCalculatorImpl : public FsiCalculator {
-public:
-
-/*
-  std::vector<double> calculator()(std::vector<double> &qinv, double Rinv) const
-    {
-      std::vector<double> K;
-      K.reserve(qinv.size());
-
-      std::transform(qinv.begin(), qinv.end(), std::back_inserter(K),
-                     [&](double q) { return static_cast<const CRTP&>(*this)(q, Rinv); });
-      //                [&](double q) { return (*this)(q, Rinv); });
-
-      return K;
-    }
-*/
-
-  static std::shared_ptr<CRTP> NewPtr()
-    {
-      return std::make_shared<CRTP>();
-    }
-
-};
-#endif
-
 
 #endif
